2083816ccfb90a9048e419b0794c047d1498c2f2
1m48 <truc.ntic@gmail.com>
2021-08-20 09:10:10 +0700

    Path

--- Graph.cpp
+++ Graph.cpp
@@ -5,70 +5,86 @@
 
 Graph::Graph(string fileName ){
     ifstream fi;
-        fi.open(fileName, ifstream::in);
-        string tmp = "";
-        getline(fi, tmp);
-        this->v = stoi(tmp);
-        this->matrix = new int*[this->v];
-        for (int i = 0; i < this->v; ++i) this->matrix[i] = new int[v];
-        char c;
-        for (int i = 0; i < this->v; ++i) {
-            int j = 0;
-            while (j < this->v) {
-                tmp = "";
-                do {
-                    c = fi.get();
-                    tmp += c; 
-                } while (c != ' ' && c != '\n' && fi.good());
-                tmp.pop_back();
-                this->matrix[i][j] = stoi(tmp);
-                ++j;
-            }
+    fi.open(fileName, ifstream::in);
+    string tmp = "";
+    getline(fi, tmp);
+    this->v = stoi(tmp);
+    this->matrix = new int*[this->v];
+    for (int i = 0; i < this->v; ++i) this->matrix[i] = new int[v];
+    char c;
+    for (int i = 0; i < this->v; ++i) {
+        int j = 0;
+        while (j < this->v) {
+            tmp = "";
+            do {
+                c = fi.get();
+                tmp += c; 
+            } while (c != ' ' && c != '\n' && fi.good());
+            tmp.pop_back();
+            this->matrix[i][j] = stoi(tmp);
+            ++j;
         }
-        fi.close();
+    }
+    fi.close();
 }
 
 Graph::~Graph() {
-        for (int i = 0; i < this->v; ++i) delete[](this->matrix[i]);
-        delete[]matrix;
-        cout << "delete successfully";
+    for (int i = 0; i < this->v; ++i) delete[](this->matrix[i]);
+    delete[]matrix;
+    cout << "delete successfully";
 }
 
-int** Graph::Dijkstra(int root) {
-        int** dijkstra = new int* [this->v + 1];
-        bool* check = new bool[this->v];
-        for (int i = 0; i <= this->v; ++i) dijkstra[i] = new int[this->v + 1]; 
-        for (int i = 0; i < this->v; ++i) check[i] = 0;
-        dijkstra[0][0] = -1;
-        int ver = root, verToRoot = 0;
-        for (int j = 1; j <= this->v; ++j)
-            if (j - 1 == root) dijkstra[0][j] = 0;
-            else dijkstra[0][j] = MAX;
-        for (int i = 1; i <= this->v; ++i) {
-            int min = MAX, minj = 0;
-            check[ver] = 1;
-            for (int j = 0; j <= this->v; ++j) {
-                if (j == 0) dijkstra[i][j] = ver;
-                else {
-                    if (this->matrix[ver][j - 1] == 0) dijkstra[i][j] = dijkstra[i - 1][j];
-                    else if (this->matrix[ver][j - 1] + verToRoot < dijkstra[i - 1][j]) dijkstra[i][j] = this->matrix[ver][j - 1] + verToRoot;
-                    else dijkstra[i][j] = dijkstra[i - 1][j];
-                    if (min >= dijkstra[i][j] && !check[j - 1]) {
-                        min = dijkstra[i][j];
-                        minj = j - 1;
-                    }
+void minPath(int** &m, int i, int j, int root) {
+    if (i == 1) {
+        cout << root;
+        return;
+    }
+    if (m[i][j] == m[i - 1][j]) minPath(m, i - 1, j, root);
+    else {
+        minPath(m, i - 1, m[i][0], root);
+        cout << "->" << m[i][0];
+    }
+    return;
+}
+
+void Graph::Dijkstra(int root) {
+    int** dijkstra = new int* [this->v + 1];            //Dijkstra matrix
+    bool* check = new bool[this->v];                    
+    for (int i = 0; i <= this->v; ++i) dijkstra[i] = new int[this->v + 1]; 
+    for (int i = 0; i < this->v; ++i) check[i] = 0;
+    dijkstra[0][0] = -1;
+    int ver = root, verToRoot = 0;
+    for (int j = 1; j <= this->v; ++j)
+        if (j - 1 == root) dijkstra[0][j] = 0;
+        else dijkstra[0][j] = MAX;
+    for (int i = 1; i <= this->v; ++i) {
+        int min = MAX, minj = 0;
+        check[ver] = 1;
+        for (int j = 0; j <= this->v; ++j) {
+            if (j == 0) dijkstra[i][j] = ver;
+            else {
+                if (this->matrix[ver][j - 1] == 0 || this->matrix[ver][j - 1] + verToRoot > dijkstra[i - 1][j]) dijkstra[i][j] = dijkstra[i - 1][j];
+                else dijkstra[i][j] = this->matrix[ver][j - 1] + verToRoot;
+                if (min >= dijkstra[i][j] && !check[j - 1]) {
+                    min = dijkstra[i][j];
+                    minj = j - 1;
                 }
             }
-            verToRoot = min;
-            ver = minj;
         }
+        verToRoot = min;
+        ver = minj;
+    }
 
-        
-        for (int i = 0; i <= this->v; ++i) {
-            for (int j = 0; j <= this->v; ++j) cout << dijkstra[i][j] << " ";
-            cout << endl;
-        }
-        return dijkstra;
+    cout << "Shortest Path from " << root << " to all" << endl;
+    cout << "Vertex \t minPath \t Path" << endl;
+    for (int j = 0; j < this->v; ++j) {
+        cout << j <<"\t " << dijkstra[this->v][j + 1] << "\t\t ";
+        minPath(dijkstra, this->v, j + 1, root);
+        if (j != root) cout << "->" << j;
+        cout << endl;
+    }
+
+
 }
 
 void Graph::display() {
